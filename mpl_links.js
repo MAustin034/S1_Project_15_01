bdef9bed062e9875b0c90b7a279086740063047a
Mickal Austin <maustin034@west-mec.org>
2019-04-18 15:23:52 -0700

    first commit

---
+++ mpl_links.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 13
+   Case Problem 1
+
+   Author: mickal Austin
+   Date: 4-18-19  
+   
+   Filename: mpl_links.js
+
+*/
+
+window.addEventListener("load", function () {
+            var allSelect = document.querySelectorAll('input[name="select"');
+
+        }
